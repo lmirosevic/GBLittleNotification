98cb272f2ec1a184bc15b27c4388a1a1c2fc009e
Luka Mirosevic <luka@goonbee.com>
2013-07-15 09:50:43 +0200

    Initial Commit

---
+++ GBLittleNotification/GBLittleNotification.h
@@ -0,0 +1,13 @@
+//
+//  GBLittleNotification.h
+//  GBLittleNotification
+//
+//  Created by Luka Mirosevic on 15/07/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBLittleNotification : NSObject
+
+@end
